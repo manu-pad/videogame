914e3d3ae6fcad9d8d42fb1a922b061468ab473f
manu-pad <madilhaaz@gmail.com>
2025-05-08 13:59:20 +0100

    designlibrarylevel

--- VideoGame/Assets/Scripts/MovingPlatform.cs
+++ VideoGame/Assets/Scripts/MovingPlatform.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class MovingPlatform : MonoBehaviour
 {
     public Transform pointA;
     public Transform pointB;
     public float moveSpeed = 2f;
+    public float waitTime = 0.03f;
 
     private Vector3 nextPosition;
+    private bool isWaiting = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +22,26 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
-
-        if (transform.position == nextPosition)
+        if (!isWaiting)
         {
-            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+
+            if (transform.position == nextPosition)
+            {
+                StartCoroutine(WaitAndSwitch());
+            }
         }
+    }
+
+    IEnumerator WaitAndSwitch()
+    {
+        isWaiting = true;
+
+        if (waitTime > 0f)
+            yield return new WaitForSeconds(waitTime);
+
+        nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+        isWaiting = false;
     }
 
 }
--- VideoGame/Assets/Scripts/WorldActions/WorldActionsController.cs
+++ VideoGame/Assets/Scripts/WorldActions/WorldActionsController.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using TMPro;
 
 public class WorldActionsController : MonoBehaviour
 {
     public Transform gateOne;
     public Transform gateTwo;
+    public Transform gateThree;
+    public Transform gateFour;
+    public Transform gateFive;
+    public Transform gateSix;
+    public Transform finalGateOne;
+
+    private Dictionary<Transform, float> initialHeights = new Dictionary<Transform, float>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        initialHeights[gateOne] = gateOne.position.y;
+        initialHeights[gateTwo] = gateTwo.position.y;
+        initialHeights[gateThree] = gateThree.position.y;
+        initialHeights[gateFour] = gateFour.position.y;
+        initialHeights[gateFive] = gateFive.position.y;
+        initialHeights[gateSix] = gateSix.position.y;
+        initialHeights[finalGateOne] = finalGateOne.position.y;
 
     }
 
@@ -18,27 +32,36 @@ public class WorldActionsController : MonoBehaviour
     void Update()
     {
         MoveGate(gateOne, "openGateOne", 2f, 5f);
-        MoveGate(gateTwo, "openGateTwo", 2f, 10f);
-
+        MoveGate(gateTwo, "openGateTwo", 2f, 5f);
+        MoveGate(gateThree, "openGateThree", 2f, 5f);
+        MoveGate(gateFour, "openGateFour", 2f, 4.5f);
+        MoveGate(gateFive, "openGateFive", 2f, 5f);
+        MoveGate(gateSix, "openGateSix", 2f, 5f);
+        MoveGate(finalGateOne, "finalGateOne", 2f, 5f);
 
     }
 
-    void MoveGate(Transform gate, string variableName, float speed, float targetHeight)
+    void MoveGate(Transform gate, string variableName, float speed, float moveAmount)
     {
-        // Verifica se a variável está ativa
         if (VariableManager.Instance.GetVariable(variableName))
         {
-            // Move o portão no eixo Y
+            // Altura inicial salva em uma variável estática ou dicionário, para cada portão
+            if (!initialHeights.ContainsKey(gate))
+            {
+                initialHeights[gate] = gate.position.y;
+            }
+
+            float targetY = initialHeights[gate] + moveAmount;
+
             gate.position += new Vector3(0, speed * Time.deltaTime, 0);
 
-            // Quando o portão atingir a altura desejada
-            if (gate.position.y >= targetHeight)
+            if (gate.position.y >= targetY)
             {
-                // Desativa a variável para parar o movimento
                 VariableManager.Instance.SetVariable(variableName, false);
             }
         }
     }
+
 
 
 }
