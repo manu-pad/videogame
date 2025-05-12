9eeda7b57c10ce1653b1eeb4ed97726bbd3226c8
manu-pad <madilhaaz@gmail.com>
2025-05-12 22:09:28 +0100

    mainstartmenu

---
+++ VideoGame/Assets/StartMenuController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenuController : MonoBehaviour
+{
+    public GameObject cutsceneImage1;
+    public GameObject cutsceneImage2;
+    public GameObject backgroundCutScene;
+    public float imageDisplayTime = 50f; // tempo em segundos
+
+    public void OnStartClick()
+    {
+        StartCoroutine(PlayCutsceneAndStart());
+    }
+
+    IEnumerator PlayCutsceneAndStart()
+    {
+        backgroundCutScene.SetActive(true);
+
+        // Mostrar imagem 1
+        cutsceneImage1.SetActive(true);
+        yield return new WaitForSeconds(imageDisplayTime);
+        cutsceneImage1.SetActive(false);
+
+        // Mostrar imagem 2
+        cutsceneImage2.SetActive(true);
+        yield return new WaitForSeconds(imageDisplayTime);
+        cutsceneImage2.SetActive(false);
+
+        // Carregar a cena
+        SceneManager.LoadScene("SubLevel1");
+    }
+
+    public void OnExitClick()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
+    }
+}
